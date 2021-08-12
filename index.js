73ccb1a92c1a933ac899ca0204a75ec545e73620
Luka Goggins <lukarowangoggins@computingforall.org>
2021-08-12 14:14:01 -0700

    added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+// var, let, or const indicates a declerations
+// when you see "=" it indicates an asignment
+var firstname = "luka";
+let lastname = "Goggins";
+const age = 17;
+
+firstname = "My Dude";
+lastname = "Homie";
+
+console.log('Hello' + ' ' + firstname + ' ' + lastname + '!' + ' You are ' + age + ' years old.');
